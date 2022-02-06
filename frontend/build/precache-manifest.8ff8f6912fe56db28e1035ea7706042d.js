821fa151e0dfc8cf17ebb161045da488d04feff0
HugoS <31020571+bysnoozy@users.noreply.github.com>
2022-02-06 15:48:36 +0100

    Add files via upload

---
+++ frontend/build/precache-manifest.8ff8f6912fe56db28e1035ea7706042d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87151ff7d56c791ec60a",
+    "url": "/static/css/main.aa5a1edb.chunk.css"
+  },
+  {
+    "revision": "87151ff7d56c791ec60a",
+    "url": "/static/js/main.87151ff7.chunk.js"
+  },
+  {
+    "revision": "f52350bba820508de3dc",
+    "url": "/static/js/1.f52350bb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1da51ee34ead0b1e7ee9a5c28e3efe05",
+    "url": "/index.html"
+  }
+];
